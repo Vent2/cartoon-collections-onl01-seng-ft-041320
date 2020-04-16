15c027a4420812d5b153afb1be220956bef16dd6
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-16 23:26:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  if array.any? {|element| element.length >= 4}
+  if array.any? {|element| element.length > 4}
     true
   elsif array.all? {|element| element.length <= 4}
     false
