1f77d0eae54be2419e7a6dee952c922437be3fc0
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-16 21:17:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
-def roll_call_dwarves(array)# code an argument here
-  array.each_with_index do |name|
-    puts ("#{name}").split
-  end
+def roll_call_dwarves(names)
+  names.each_with_index()
+
+
 end
 
 def summon_captain_planet# code an argument here
