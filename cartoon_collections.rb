ab30fb749230036bf5692a142ec961fab984ac71
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-16 23:10:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 
 def summon_captain_planet(array)
-  array.each{|n, index| return (n[index])}.upcase(0)
+  array.each{|n, index| return (n[index]).upcase(0)}
 end
 
 def long_planeteer_calls# code an argument here
